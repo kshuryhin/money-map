dc5edf5f29130c7ed1ba2ef3d4f1be871f15b7cc
kshuryhin <ksurygin5@gmail.com>
2024-06-24 15:13:48 +0200

    test gh rules

--- api/src/main/java/com/money/map/api/ApiApplication.java
+++ api/src/main/java/com/money/map/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.money.map.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// for testing
 @SpringBootApplication
 public class ApiApplication {
 
